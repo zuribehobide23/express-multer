e45ecfeafa1722cb817ded62de44c5651361fea1
ZURIÑE BEHOBIDE ZABALEGUI <157796849+zuribehobide23@users.noreply.github.com>
2024-12-10 10:54:45 +0000

    hasten

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+//const upload = multer({ dest: 'uploads/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+const upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
