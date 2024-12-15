9bc30659de14918ae676777c1c0c5e2fdae05bc2
ZURIÑE BEHOBIDE ZABALEGUI <157796849+zuribehobide23@users.noreply.github.com>
2024-12-15 12:16:00 +0000

    aldaketak

--- routes/profile.js
+++ routes/profile.js
@@ -4,8 +4,8 @@ const multer  = require('multer')
 //const upload = multer({ dest: 'uploads/' })
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads')
+    dest: function (req, file, cb) {
+      cb(null, 'uploads/')
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
